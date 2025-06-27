1eee7e16fb3a42a7f32a03e27ef2f2cd6460c01f
Christian Fernandes <7christianfsantos@gmail.com>
2025-06-27 16:26:38 -0300

    Finalizado

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let botao = document.querySelector("#botao");
 let mensagem = document.querySelector("#mensagem");
 let rodape = document.querySelector("#rodape");
 
-    botao.addEventListener('click', mudanca)
+    botao.addEventListener('click', mudanca);
     function mudanca(){
         mensagem.innerHTML="Não adie mais a vida para a qual você está destinado. Assuma a responsabilidade e a construa!";
         rodape.style.height="15vh";
